409a83f49f130d12b9724bff7c52043e9e6cca99
Banana Viking <bananavikingcoder@gmail.com>
2018-04-30 14:54:40 -0500

    add parameter to play bird sound method

--- WordOfTheBird/ViewController.swift
+++ WordOfTheBird/ViewController.swift
@@ -14,27 +14,27 @@ class ViewController: UIViewController {
     var player: AVAudioPlayer?
 
     @IBAction func bird0Tapped(_ sender: UIButton) {
-        playGoldFinchCallSound()
+        playBirdSound(forBird: "goldFinch")
     }
     
     @IBAction func bird1Tapped(_ sender: UIButton) {
-        playBaldEagleCallSound()
+        playBirdSound(forBird: "baldEagle")
     }
     
     @IBAction func bird2Tapped(_ sender: UIButton) {
-        playBlueJayCallSound()
+        playBirdSound(forBird: "blueJay")
     }
     
     @IBAction func bird3Tapped(_ sender: UIButton) {
-        playCardinalCallSound()
+        playBirdSound(forBird: "cardinal")
     }
     
     @IBAction func bird4Tapped(_ sender: UIButton) {
-        playHouseFinchCallSound()
+        playBirdSound(forBird: "houseFinch")
     }
     
     @IBAction func bird5Tapped(_ sender: UIButton) {
-        playRedTailedHawkCallSound()
+        playBirdSound(forBird: "redTailedHawk")
     }
     
     override func viewDidLoad() {
@@ -47,103 +47,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func playGoldFinchCallSound() {
-        guard let url = Bundle.main.url(forResource: "goldFinchCall", withExtension: "wav") else {
-            print("url not found")
-            return
-        }
-        
-        do {
-            /// this codes for making this app ready to takeover the device audio
-            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
-            try AVAudioSession.sharedInstance().setActive(true)
-            
-            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
-            
-            player!.play()
-        } catch let error as NSError {
-            print("error: \(error.localizedDescription)")
-        }
-    }
-    
-    func playBaldEagleCallSound() {
-        guard let url = Bundle.main.url(forResource: "baldEagleCall", withExtension: "wav") else {
-            print("url not found")
-            return
-        }
-        
-        do {
-            /// this codes for making this app ready to takeover the device audio
-            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
-            try AVAudioSession.sharedInstance().setActive(true)
-            
-            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
-            
-            player!.play()
-        } catch let error as NSError {
-            print("error: \(error.localizedDescription)")
-        }
-    }
-
-    func playBlueJayCallSound() {
-        guard let url = Bundle.main.url(forResource: "blueJayCall", withExtension: "wav") else {
-            print("url not found")
-            return
-        }
-        
-        do {
-            /// this codes for making this app ready to takeover the device audio
-            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
-            try AVAudioSession.sharedInstance().setActive(true)
-            
-            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
-            
-            player!.play()
-        } catch let error as NSError {
-            print("error: \(error.localizedDescription)")
-        }
-    }
-    
-    func playCardinalCallSound() {
-        guard let url = Bundle.main.url(forResource: "cardinalCall", withExtension: "wav") else {
-            print("url not found")
-            return
-        }
-        
-        do {
-            /// this codes for making this app ready to takeover the device audio
-            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
-            try AVAudioSession.sharedInstance().setActive(true)
-            
-            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
-            
-            player!.play()
-        } catch let error as NSError {
-            print("error: \(error.localizedDescription)")
-        }
-    }
-    
-    func playHouseFinchCallSound() {
-        guard let url = Bundle.main.url(forResource: "houseFinchCall", withExtension: "wav") else {
-            print("url not found")
-            return
-        }
-        
-        do {
-            /// this codes for making this app ready to takeover the device audio
-            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
-            try AVAudioSession.sharedInstance().setActive(true)
-            
-            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
-            
-            player!.play()
-        } catch let error as NSError {
-            print("error: \(error.localizedDescription)")
-        }
-    }
-    
-    func playRedTailedHawkCallSound() {
-        guard let url = Bundle.main.url(forResource: "redTailedHawkCall", withExtension: "wav") else {
+    func playBirdSound(forBird: String) {
+        guard let url = Bundle.main.url(forResource: forBird, withExtension: "wav") else {
             print("url not found")
             return
         }
