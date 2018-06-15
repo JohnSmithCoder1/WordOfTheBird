48c904691e21bb294730d4091c1da30a14971b46
derek3little <derek3little@gmail.com>
2018-06-15 16:53:11 -0400

    hooked up all bird segues, fixed separator insets

--- WordOfTheBird/View Controllers/BirdsViewController.swift
+++ WordOfTheBird/View Controllers/BirdsViewController.swift
@@ -34,16 +34,126 @@ class BirdsViewController: UITableViewController {
                 destination.callDetail = americanGoldfinch.call
                 destination.linkDetail = americanGoldfinch.link
             }
+        } else if segue.identifier == "americanKestrelSegue" {
+            if let destination = segue.destination as? BirdDetailViewController {
+                destination.title = americanKestrel.name
+                destination.imageDetail = americanKestrel.imageLarge
+                destination.callDetail = americanKestrel.call
+                destination.linkDetail = americanKestrel.link
+            }
+        } else if segue.identifier == "baldEagleSegue" {
+            if let destination = segue.destination as? BirdDetailViewController {
+                destination.title = baldEagle.name
+                destination.imageDetail = baldEagle.imageLarge
+                destination.callDetail = baldEagle.call
+                destination.linkDetail = baldEagle.link
+            }
+        } else if segue.identifier == "barredOwlSegue" {
+            if let destination = segue.destination as? BirdDetailViewController {
+                destination.title = barredOwl.name
+                destination.imageDetail = barredOwl.imageLarge
+                destination.callDetail = barredOwl.call
+                destination.linkDetail = barredOwl.link
+            }
+        } else if segue.identifier == "blackCappedChickadeeSegue" {
+            if let destination = segue.destination as? BirdDetailViewController {
+                destination.title = blackCappedChickadee.name
+                destination.imageDetail = blackCappedChickadee.imageLarge
+                destination.callDetail = blackCappedChickadee.call
+                destination.linkDetail = blackCappedChickadee.link
+            }
+        } else if segue.identifier == "blueJaySegue" {
+            if let destination = segue.destination as? BirdDetailViewController {
+                destination.title = blueJay.name
+                destination.imageDetail = blueJay.imageLarge
+                destination.callDetail = blueJay.call
+                destination.linkDetail = blueJay.link
+            }
+        } else if segue.identifier == "cardinalSegue" {
+            if let destination = segue.destination as? BirdDetailViewController {
+                destination.title = cardinal.name
+                destination.imageDetail = cardinal.imageLarge
+                destination.callDetail = cardinal.call
+                destination.linkDetail = cardinal.link
+            }
+        } else if segue.identifier == "downyWoodpeckerSegue" {
+            if let destination = segue.destination as? BirdDetailViewController {
+                destination.title = downyWoodpecker.name
+                destination.imageDetail = downyWoodpecker.imageLarge
+                destination.callDetail = downyWoodpecker.call
+                destination.linkDetail = downyWoodpecker.link
+            }
+        } else if segue.identifier == "greatHornedOwlSegue" {
+            if let destination = segue.destination as? BirdDetailViewController {
+                destination.title = greatHornedOwl.name
+                destination.imageDetail = greatHornedOwl.imageLarge
+                destination.callDetail = greatHornedOwl.call
+                destination.linkDetail = greatHornedOwl.link
+            }
+        } else if segue.identifier == "hairyWoodpeckerSegue" {
+            if let destination = segue.destination as? BirdDetailViewController {
+                destination.title = hairyWoodpecker.name
+                destination.imageDetail = hairyWoodpecker.imageLarge
+                destination.callDetail = hairyWoodpecker.call
+                destination.linkDetail = hairyWoodpecker.link
+            }
+        } else if segue.identifier == "houseFinchSegue" {
+            if let destination = segue.destination as? BirdDetailViewController {
+                destination.title = houseFinch.name
+                destination.imageDetail = houseFinch.imageLarge
+                destination.callDetail = houseFinch.call
+                destination.linkDetail = houseFinch.link
+            }
+        } else if segue.identifier == "northernFlickerSegue" {
+            if let destination = segue.destination as? BirdDetailViewController {
+                destination.title = northernFlicker.name
+                destination.imageDetail = northernFlicker.imageLarge
+                destination.callDetail = northernFlicker.call
+                destination.linkDetail = northernFlicker.link
+            }
+        } else if segue.identifier == "peregrineFalconSegue" {
+            if let destination = segue.destination as? BirdDetailViewController {
+                destination.title = peregrineFalcon.name
+                destination.imageDetail = peregrineFalcon.imageLarge
+                destination.callDetail = peregrineFalcon.call
+                destination.linkDetail = peregrineFalcon.link
+            }
+        } else if segue.identifier == "pileatedWoodpeckerSegue" {
+            if let destination = segue.destination as? BirdDetailViewController {
+                destination.title = pileatedWoodpecker.name
+                destination.imageDetail = pileatedWoodpecker.imageLarge
+                destination.callDetail = pileatedWoodpecker.call
+                destination.linkDetail = pileatedWoodpecker.link
+            }
+        } else if segue.identifier == "redBelliedWoodpeckerSegue" {
+            if let destination = segue.destination as? BirdDetailViewController {
+                destination.title = redBelliedWoodpecker.name
+                destination.imageDetail = redBelliedWoodpecker.imageLarge
+                destination.callDetail = redBelliedWoodpecker.call
+                destination.linkDetail = redBelliedWoodpecker.link
+            }
+        } else if segue.identifier == "redHeadedWoodpeckerSegue" {
+            if let destination = segue.destination as? BirdDetailViewController {
+                destination.title = redHeadedWoodpecker.name
+                destination.imageDetail = redHeadedWoodpecker.imageLarge
+                destination.callDetail = redHeadedWoodpecker.call
+                destination.linkDetail = redHeadedWoodpecker.link
+            }
+        } else if segue.identifier == "redTailedHawkSegue" {
+            if let destination = segue.destination as? BirdDetailViewController {
+                destination.title = redTailedHawk.name
+                destination.imageDetail = redTailedHawk.imageLarge
+                destination.callDetail = redTailedHawk.call
+                destination.linkDetail = redTailedHawk.link
+            }
+        } else if segue.identifier == "tuftedTitmouseSegue" {
+            if let destination = segue.destination as? BirdDetailViewController {
+                destination.title = tuftedTitmouse.name
+                destination.imageDetail = tuftedTitmouse.imageLarge
+                destination.callDetail = tuftedTitmouse.call
+                destination.linkDetail = tuftedTitmouse.link
+            }
         }
-        
-//        if segue.identifier == "cardinal" {
-//            if let destination = segue.destination as? BirdDetailViewController {
-//                destination.title = cardinal.name
-//                destination.detailImage = cardinal.imageLarge
-//                destination.call = cardinal.call
-//                destination.link = cardinal.link
-//            }
-//        }
     }
     
     let americanGoldfinch = Bird(name: "American Goldfinch",
