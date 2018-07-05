3789c7294788db82417d03811346970c7162d981
Banana Viking <bananavikingcoder@gmail.com>
2018-07-05 14:05:45 -0500

    got search bar working

--- WordOfTheBird/View Controllers/BirdsViewController.swift
+++ WordOfTheBird/View Controllers/BirdsViewController.swift
@@ -15,17 +15,19 @@ class BirdTableViewCell: UITableViewCell {
 
 class BirdsViewController: UITableViewController,  UISearchBarDelegate {
     
+    var filteredBirds: [Bird]!
+    
     @IBOutlet weak var searchBar: UISearchBar!
     
     // for UITableView data source protocol
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return birdArray.count
+        return filteredBirds.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell")! as! BirdTableViewCell
-        cell.birdCellLabel?.text = birdArray[indexPath.row].name
-        cell.birdCellImage?.image = birdArray[indexPath.row].imageSmall
+        cell.birdCellLabel?.text = filteredBirds[indexPath.row].name
+        cell.birdCellImage?.image = filteredBirds[indexPath.row].imageSmall
         return cell
     }
     
@@ -35,16 +37,26 @@ class BirdsViewController: UITableViewController,  UISearchBarDelegate {
         let backgroundImage = UIImageView(image: UIImage(named: "background.png"))
         backgroundImage.frame = self.tableView.frame
         self.tableView.backgroundView = backgroundImage
+        filteredBirds = birdArray
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        // When user has entered text into the search box use the filter method to iterate over all items in the data array for each item, return true if the item should be included and false if the item should NOT be included
+        filteredBirds = searchText.isEmpty ? birdArray : birdArray.filter { (item: Bird) -> Bool in
+            // If dataItem matches the searchText, return true to include it
+            return item.name.range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil
+        }
+        tableView.reloadData()
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
             if let destination = segue.destination as? BirdDetailViewController {
                 let myIndexPath = self.tableView.indexPathForSelectedRow!
                 let row = myIndexPath.row
-                destination.title = birdArray[row].name
-                destination.imageDetail = birdArray[row].imageLarge
-                destination.callDetail = birdArray[row].call
-                destination.linkDetail = birdArray[row].link
+                destination.title = filteredBirds[row].name
+                destination.imageDetail = filteredBirds[row].imageLarge
+                destination.callDetail = filteredBirds[row].call
+                destination.linkDetail = filteredBirds[row].link
             }
         }
     }
