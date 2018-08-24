608984d80fe2130ef93ba92b38d5ccb4e1a6bbc3
Banana Viking <bananavikingcoder@gmail.com>
2018-08-23 21:58:51 -0500

    add blank AboutVC

---
+++ WordOfTheBird/Controller/AboutViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.swift
+//  WordOfTheBird
+//
+//  Created by Banana Viking on 8/23/18.
+//  Copyright © 2018 Banana Viking. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+}
