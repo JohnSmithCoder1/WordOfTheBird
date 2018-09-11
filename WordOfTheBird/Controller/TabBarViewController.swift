569bd78d5c5a5705e0cd68c8c55d2b08f22898a8
derek3little <derek3little@gmail.com>
2018-09-11 13:02:35 -0400

    adjusted colors of nav and tab bars

---
+++ WordOfTheBird/Controller/TabBarViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TabBarController.swift
+//  WordOfTheBird
+//
+//  Created by Derek E Little on 9/11/18.
+//  Copyright © 2018 Banana Viking. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+    override func viewDidLoad() {
+        tabBar.unselectedItemTintColor = UIColor(red: 235, green: 235, blue: 235, alpha: 0.5)
+    }
+}
+
